fb9ba1049f887d8fb5a2f9fae9854cbe6ce84594
jungyunmok <jungyunmok@gmail.com>
2023-03-04 22:44:18 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        if(n >= 10){
+            int service = n/10;
+            answer = (12000*n) + (2000*(k-service));
+        } else {
+            answer = (12000*n) + (2000*k);
+        }
+        
+        return answer;
+    }
+}
