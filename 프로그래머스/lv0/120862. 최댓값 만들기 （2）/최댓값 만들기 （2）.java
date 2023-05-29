290d30bd72bb7b1e7484958bca93333b28cddf49
jungyunmok <jungyunmok@gmail.com>
2023-05-29 21:15:44 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = -100000000;
+        for(int i=0; i<numbers.length; i++) {
+            for(int j=i+1; j<numbers.length; j++) {
+                answer = numbers[i]*numbers[j] > answer ? numbers[i]*numbers[j] : answer;
+            }
+        }
+        return answer;
+    }
+}
