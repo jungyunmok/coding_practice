82cc56a9fb5410f14cf302228fca98597964b4fb
jungyunmok <jungyunmok@gmail.com>
2023-08-08 16:05:37 +0900

    [Bronze V] Title: A+B - 8, Time: 284 ms, Memory: 19000 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int t = scan.nextInt();
+    for (int i = 0; i < t; i++) {
+      int a = scan.nextInt();
+      int b = scan.nextInt();
+      System.out.println("Case #" + (i+1) + ": " + a + " + " + b + " = " + (a+b));
+    }
+  }
+}
