7bf11d9cc24e9618faddf96dabe6e2490a06d469
jungyunmok <jungyunmok@gmail.com>
2024-04-08 16:22:50 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int num = input.charAt(0);
+        System.out.println(num);
+    }
+}
